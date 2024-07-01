544ad08e665b3017117fa1b10c0c8846995a1eb9
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2024-06-30 23:10:39 -0300

    fix: correção de SEO

--- hora-da-tecnologia/src/Shared/Components/Card/index.jsx
+++ hora-da-tecnologia/src/Shared/Components/Card/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import {
   CardWrapper,
   CardTitle,
@@ -10,6 +11,18 @@ import {
 
 const Card = ({ titulo, imagem, categoria, descricao, link }) => (
   <CardWrapper>
+    <Helmet>
+      <title>{titulo}</title>
+      <meta name="description" content={descricao} />
+      <meta property="og:title" content={titulo} />
+      <meta property="og:description" content={descricao} />
+      <meta property="og:image" content={imagem} />
+      <meta property="og:url" content={`https://https://www.horadatecnologia.com.br/${link}`} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={titulo} />
+      <meta name="twitter:description" content={descricao} />
+      <meta name="twitter:image" content={imagem} />
+    </Helmet>
     <CardImage src={imagem} />
     <CardCategoria>{categoria}</CardCategoria>
     <CardTitle>{titulo}</CardTitle>
