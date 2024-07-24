60ad3f87bcef3f4bde143ea04d01352e509632b5
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2024-07-24 17:48:20 -0300

    fix: ajustes de mock

--- hora-da-tecnologia/src/Views/Tutorial/index.jsx
+++ hora-da-tecnologia/src/Views/Tutorial/index.jsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Helmet } from "react-helmet";
 import { FaTwitter, FaFacebook, FaLinkedin, FaWhatsapp } from "react-icons/fa";
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css"; // Importa o tema do PrismJS
 import {
   PostWrapper,
   PostTitle,
@@ -28,6 +30,13 @@ const Tutorial = () => {
         setError("Erro ao carregar o tutorial. Tente novamente mais tarde.");
       });
   }, [slug]);
+
+  useEffect(() => {
+    // Destaque o código sempre que o conteúdo for atualizado
+    if (post) {
+      Prism.highlightAll();
+    }
+  }, [post]);
 
   const handleShare = (network) => {
     if (!post) return;
@@ -86,10 +95,13 @@ const Tutorial = () => {
         .map((paragraph, index) => <p key={index}>{paragraph}</p>);
     };
 
-    const id = post.id;
-
     const createParagraphsFormat = () => {
-      return <div className="post-content" dangerouslySetInnerHTML={{ __html: post.conteudo }} />;
+      const codeBlockRegex = /```(\w+)?\n([\s\S]*?)```/g;
+      const htmlWithCode = post.conteudo.replace(codeBlockRegex, (match, lang, code) => {
+        return `<pre><code class="language-${lang || 'javascript'}">${Prism.highlight(code, Prism.languages[lang || 'javascript'], lang || 'javascript')}</code></pre>`;
+      });
+
+      return <div className="post-content" dangerouslySetInnerHTML={{ __html: htmlWithCode }} />;
     };
 
     return (
@@ -97,32 +109,20 @@ const Tutorial = () => {
         <Helmet>
           <title>Hora da Tecnologia - {post.titulo}</title>
           <meta name="description" content={post.descricao} />
-          <meta
-            name="keywords"
-            content="palavra-chave1, palavra-chave2, palavra-chave3"
-          />
+          <meta name="keywords" content="palavra-chave1, palavra-chave2, palavra-chave3" />
           <meta property="og:title" content={post.titulo} />
           <meta property="og:description" content={post.descricao} />
           <meta property="og:image" content={post.imagem} />
           <meta property="og:image:secure_url" content={post.imagem} />
-          <meta
-            property="og:url"
-            content={`https://www.horadatecnologia.com.br/${slug}`}
-          />
+          <meta property="og:url" content={`https://www.horadatecnologia.com.br/${slug}`} />
           <meta property="og:type" content="article" />
-          <meta
-            property="article:published_time"
-            content={post.dataPublicacao}
-          />
+          <meta property="article:published_time" content={post.dataPublicacao} />
           <meta property="article:author" content="Nome do Autor" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content={post.titulo} />
           <meta name="twitter:description" content={post.descricao} />
           <meta name="twitter:image" content={post.imagem} />
-          <link
-            rel="canonical"
-            href={`https://www.horadatecnologia.com.br/${slug}`}
-          />
+          <link rel="canonical" href={`https://www.horadatecnologia.com.br/${slug}`} />
           <meta name="robots" content="index, follow" />
           <meta name="googlebot" content="index, follow" />
           <meta name="referrer" content="origin-when-crossorigin" />
@@ -130,7 +130,7 @@ const Tutorial = () => {
         </Helmet>
         <img src={post.imagem} alt={post.titulo} />
         <PostTitle>{post.titulo}</PostTitle>
-        {id === 0 || id === 5 || id === 6 || id === 7 ? createParagraphsFormat() : <PostContent>{createParagraphs()}</PostContent>}
+        {post.id === 0 || post.id === 5 || post.id === 6 || post.id === 7 ? createParagraphsFormat() : <PostContent>{createParagraphs()}</PostContent>}
         <SharedTitle>Compartilhe nas redes sociais</SharedTitle>
         <ShareButton network="twitter" onClick={() => handleShare("twitter")}>
           <FaTwitter /> Compartilhar no Twitter
